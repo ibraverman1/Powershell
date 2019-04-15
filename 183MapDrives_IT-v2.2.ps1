e99f3e947f971523c8ce34f118b6eb54927bc93c
ibraverman1 <49649748+ibraverman1@users.noreply.github.com>
2019-04-15 11:13:55 -0400

    Update 183MapDrives_IT-v2.2.ps1

--- 183MapDrives_IT-v2.2.ps1
+++ 183MapDrives_IT-v2.2.ps1
@@ -6,7 +6,7 @@ $unm = $env:USERNAME
 $cnm = $env:COMPUTERNAME
 $udom = $env:USERDOMAIN
 
-$DepartmentsRoot = "\\L183FS01\Departments"
+$DepartmentsRoot = "\\L183FS01.LOCAL183.TOR\Departments"
 $ITShared = "$DepartmentsRoot\IT\Shared Docs"
 $AccountingShard = "$DepartmentsRoot\Accounting\Shared Docs"
 $LegalShared = "$DepartmentsRoot\Legal\Shared Docs"
@@ -19,7 +19,7 @@ $cred = Get-Credential -UserName $udom\$unm -Message "Please enter your password
 
 183 simple
 
-New-PSDrive -Name "T" -PSProvider FileSystem -Description "IT test" -Root "\\L183FS01\Departments\It\Shared Docs" -Persist
+New-PSDrive -Name "T" -PSProvider FileSystem -Description "IT test" -Root "\\L183FS01.LOCAL183.TOR\Departments\It\Shared Docs" -Persist
 (New-Object -ComObject Shell.Application).NameSpace("T:").Self.Name="IT TEST DRIVE"
 #>
 
@@ -27,12 +27,12 @@ Remove-PSDrive -Name Z -Scope global
 
 if ($userName -eq "ittester")
 {
-    New-PSDrive -Name "Z" -PSProvider FileSystem -Description "IT SHARED DOCS" -Root "\\L183FS01\Departments\IT\Shared Docs" -Credential $cred -Persist -scope global
+    New-PSDrive -Name "Z" -PSProvider FileSystem -Description "IT SHARED DOCS" -Root "\\L183FS01.LOCAL183.TOR\Departments\IT\Shared Docs" -Credential $cred -Persist -scope global
     (New-Object -ComObject Shell.Application).NameSpace("Z:").Self.Name="IT Shared Docs"
   
 }
 elseif (($userName -eq "ibraverman") -or ($userName -eq "pgongalves") -or ($userName -eq "kchanderpaul") -or ($userName -eq "dcosta"))
 {
-   New-PSDrive -Name "Z:" -PSProvider FileSystem -Description "IT test" -Root "\\L183FS01\Departments\IT\Shared Docs" -Credential $cred -Persist -scope global
+   New-PSDrive -Name "Z:" -PSProvider FileSystem -Description "IT test" -Root "\\L183FS01.LOCAL183.TOR\Departments\IT\Shared Docs" -Credential $cred -Persist -scope global
    (New-Object -ComObject Shell.Application).NameSpace("Z:").Self.Name="IT Shared Docs"  
 }
