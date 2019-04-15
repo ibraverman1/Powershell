6b6e372e73e62788e58a5940768de64f3fceb61a
ibraverman1 <49649748+ibraverman1@users.noreply.github.com>
2019-04-15 10:51:32 -0400

    Add files via upload

---
+++ 183MapPrinters-IT_v.2.1.ps1
@@ -0,0 +1,36 @@
+﻿#### Remove All Network Printers
+Foreach($NetworkPrinter in $NetworkPrinters) 
+            { 
+                $NetworkPrinter.Delete() 
+                Write-Host "Successfully deleted the network printer:" + $NetworkPrinter.Name -ForegroundColor Green     
+            }
+
+#### Map Printers per Location
+
+$userName = $env:USERNAME
+$compName = $env:COMPUTERNAME
+$defaultGateway = (Get-wmiObject Win32_networkAdapterConfiguration | ?{$_.IPEnabled}).DefaultIPGateway
+
+if ($defaultGateway -eq "10.250.180.1")
+{    
+    #Add-Printer -ConnectionName \\l183fs03.local183.tor\HPM477_IT
+    #Add-Printer -ConnectionName \\l183fs03.local183.tor\HPM452_IT
+    #Add-Printer -ConnectionName \\l183fs03.local183.tor\Canon6555_Accounting
+
+    if ($userName -eq "ittester")
+    {
+        & rundll32.exe printui.dll,PrintUIEntry /y /n "HPM477_IT"
+    }
+    elseif ($userName -eq "ibraverman")
+    {
+        & rundll32.exe printui.dll,PrintUIEntry /y /n "HPM477_IT"
+    }
+    elseif (($userName -eq "pgoncalves") -or ($userName -eq "kchanderpaul"))
+    {
+        & rundll32.exe printui.dll,PrintUIEntry /y /n "HPM452_IT"
+    }
+    else 
+    {
+        & rundll32.exe printui.dll,PrintUIEntry /y /n "Canon6555_Accounting"
+    } 
+}
